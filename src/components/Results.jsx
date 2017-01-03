9589e62818bb08507e12ea04b527fb5c99852cf2
Andre Stackhouse <CaptainStack@outlook.com>
2017-01-02 16:20:06 -0800

    Finish client UI.

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -13,16 +13,27 @@ export default React.createClass({
     return 0;
   },
   render: function() {
-    return <div className="results">
-      {this.getPair().map(entry =>
-        <div key={entry} className="entry">
-          <h1>{entry}</h1>
-          <div className="voteCount">
-            {this.getVotes(entry)}
-          </div>
+    return this.props.winner ?
+      <Winner ref="winner" winner={this.props.winner} /> :
+      <div className="results">
+        <div classname="tally">
+          {this.getPair().map(entry =>
+            <div key={entry} className="entry">
+              <h1>{entry}</h1>
+              <div className="voteCount">
+                {this.getVotes(entry)}
+             </div>
+           </div>
+          )}
         </div>
-      )}
-    </div>
+        <div className="management">
+          <button ref="next"
+                  className="next"
+                  onClick={this.props.next}>
+            Next
+          </button>
+        </div>
+      </div>
   }
 });
 
